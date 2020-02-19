f3650a9c8f6b30742742c6aa13b67e41b89dcc63
alexisFdz19 <alexisFdz_19@outlook.com>
2020-02-19 11:37:36 -0500

    Corrección de bug
    -Corrección de bug al cargar las fotografías

--- js/mostrarFotos.js
+++ js/mostrarFotos.js
@@ -4,8 +4,114 @@ jQuery(document).ready(function(){
 
 });
 
-/*$('.botonFiltro').click(function(){
+$('.botonFiltroCL').click(function(){
 
-    $('.oculto').show();
+    $('.ap').show();
+    $('.ap1').hide();
+    $('.ap2').hide();
+    $('.ap3').hide();
+    $('.ap4').hide();
+    $('.ap5').hide();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').hide();
 
-});*/
+})
+
+$('.botonFiltroPL').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').show();
+    $('.ap2').hide();
+    $('.ap3').hide();
+    $('.ap4').hide();
+    $('.ap5').hide();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').hide();
+
+})
+
+$('.botonFiltroWL').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').hide();
+    $('.ap2').show();
+    $('.ap3').hide();
+    $('.ap4').hide();
+    $('.ap5').hide();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').hide();
+
+})
+
+$('.botonFiltroTU').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').hide();
+    $('.ap2').hide();
+    $('.ap3').show();
+    $('.ap4').hide();
+    $('.ap5').hide();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').hide();
+
+})
+
+$('.botonFiltroPR').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').hide();
+    $('.ap2').hide();
+    $('.ap3').hide();
+    $('.ap4').show();
+    $('.ap5').hide();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').hide();
+
+})
+
+$('.botonFiltroGB').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').hide();
+    $('.ap2').hide();
+    $('.ap3').hide();
+    $('.ap4').hide();
+    $('.ap5').show();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').hide();
+
+})
+
+$('.botonFiltroMA').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').hide();
+    $('.ap2').hide();
+    $('.ap3').hide();
+    $('.ap4').hide();
+    $('.ap5').hide();
+    $('.ap6').show();
+    $('.ap7').hide();
+    $('.ap8').hide();
+
+})
+
+$('.botonFiltroWA').click(function(){
+
+    $('.ap').hide();
+    $('.ap1').hide();
+    $('.ap2').hide();
+    $('.ap3').hide();
+    $('.ap4').hide();
+    $('.ap5').hide();
+    $('.ap6').hide();
+    $('.ap7').hide();
+    $('.ap8').show();
+
+})
