12425dd7de304ae9406b3d6414ef23d379043a7c
Alexis Fernández <alexisFdz_19@outlook.com>
2024-04-10 12:10:29 -0500

    Fix página beneficios en el hosting
    Código condicional para que no se ejecuté la plantilla en el hosting y resolver la página tarjeta beneficios

--- controllers/plantilla.controlador.php
+++ controllers/plantilla.controlador.php
@@ -10,3 +10,23 @@ class ControladorPlantilla{
 	}
 
 }
+
+
+// Código condicional para que no se ejecuté la plantilla en el hosting y resolver la página tarjeta beneficios
+
+/* class ControladorPlantilla{
+
+    public function plantilla(){
+
+        // Verificar si la página actual es 'tarjeta-beneficios'
+        if (isset($_GET["ruta"]) && $_GET["ruta"] === "tarjeta-beneficios") {
+            // No incluir la plantilla si la página es 'tarjeta-beneficios'
+            return;
+        }
+
+        // Incluir la plantilla solo si no es la página 'tarjeta-beneficios'
+        include "views/plantilla.php";
+
+    }
+
+}*/
